1144798de345adc327e98396c00ecb27c904c8ec
wuxinkai <275648208@qq.com>
2021-12-22 14:09:09 +0800

    1

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http');
 let users = [
-    {id:22,name:'zhufeng1'},
-    {id:22,name:'zhufeng2'},
-    {id:22,name:'zhufeng3'}
+    {id:1,name:'zhufeng1'},
+    {id:2,name:'zhufeng2'},
+    {id:3,name:'zhufeng3'}
 ]
 let server = http.createServer(function(req,res){
   res.setHeader('Access-Control-Allow-Origin','*');
